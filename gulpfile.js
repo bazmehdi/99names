60298d1c815d8efe8fd9d1881853f8d64a0992b2
Baz Mehdi <basim96@hotmail.co.uk>
2019-08-19 17:28:49 +0100

    Gulp minify all css, Started working on uglify JS but need to postpone as encountered error in fetching all the .js files

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,24 @@
 // dependencies
 
 var gulp = require('gulp');
+const babel = require('gulp-babel');
+const plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-clean-css');
-var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+const terser = require('terser');
+const composer = require('gulp-uglify/composer');
+const uglify = composer(terser, console);
 var rename = require('gulp-rename');
 var changed = require('gulp-changed');
+
 
 ///////////////
 // - SCSS/CSS
 ///////////////
 
-var SCSS_SRC = './src/App.scss';
-var SCSS_DEST = './src/css';
+var SCSS_SRC = ['./src/App.scss','./src/components/**/*.scss'];
+var SCSS_DEST = './dist/css';
 
 // Compile SCSS
 gulp.task('compile_scss', function(cb){
@@ -26,7 +32,7 @@ gulp.task('compile_scss', function(cb){
     .pipe(changed(SCSS_DEST))
     .pipe(gulp.dest(SCSS_DEST));
     cb()
-        
+
 });
         
 // Detect changes in SCSS
@@ -34,6 +40,36 @@ gulp.task('watch_scss', function(cb){
     gulp.watch(SCSS_SRC, gulp.series('compile_scss'));
     cb()
 });
+
+/* WIP
+
+///////////////
+// - JS
+///////////////
+
+var JS_SRC = ['./server.js','./src/*.js','./src/components/*.js','./src/components/FOLDER_NAME/*.js'];
+var JS_DEST = './dist/js';
+
+// Compile JS
+gulp.task('compile_js', function(cb){
+
+    gulp.src(JS_SRC)
+    .pipe(babel({presets: ['@babel/preset-env']}))
+    .pipe(uglify().on('error', function(e){ console.log(e); }))
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(changed(JS_DEST))
+    .pipe(gulp.dest(JS_DEST));
+    cb()
+
+});
+        
+// Detect changes in JS
+gulp.task('watch_js', function(cb){
+    gulp.watch(JS_SRC, gulp.series('compile_js'));
+    cb()
+});
+
+*/
         
 // Run tasks
-gulp.task('default', gulp.series('watch_scss'));
+gulp.task('default', gulp.series('watch_scss'/*,'watch_js'*/));
